cb5ba21b2805afb31492a1df5309b3cb8f50394d
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2016-12-01 14:53:55 +0100

    add adaptiveRANSEC, modifiy interface and notation of RANSAC functions

--- ex05/adaptiveRANSAC.m
+++ ex05/adaptiveRANSAC.m
@@ -1,11 +1,10 @@
-function  [X1_inline, X2_inline]= naiveRANSAC(X1, X2, func_trans, func_dist, nb_sample, t, T, N)
+function  [X1_inline, X2_inline]= adaptiveRANSAC(X1, X2, func_trans, func_dist, s, t, p)
     % X1, X2 pairs to be matched
     % func_trans the transformation function (input 2 matrices of matched points output transformation matrix)
     % func_dist the distance function (input 2 matrices of matched points output 1D vector of distances)
-    % nb_sample the minimum number of samples that the func_trans needs (s)
-    % t the threshold for inliner distance
-    % T the threshold for the number of the inliner of the samples
-    % N the maximum number of trials
+    % s the minimum number of samples that the func_trans needs (s)
+    % t the threshold for inliner distance (t)
+    % p the threshold for the proportion of the inliner of the samples
     % X1_inline, X2_inline the found inline points
     
     % check that X1 and X2 have the same shape
@@ -14,31 +13,30 @@ function  [X1_inline, X2_inline]= naiveRANSAC(X1, X2, func_trans, func_dist, nb_
     end
     % get number of pairs and inliner thrershold
     [num, ~] = size(X1);
-    in_t = round(num * T);
     % initialize consensus set
     X1_inline = [];
     X2_inline = [];
     nb_inline = 0;
     % start trial
     trial = 0;
+    N = intmax;
     while trial < N
         % get transformation matrix from random sampling
-        indx = randsample(num, nb_sample);
+        indx = randsample(num, s);
         M = func_trans(X1(indx,:), X2(indx,:));
         % TODO catch error caused by degeneration?
         % get the corresponding consensus set
         dists = func_dist(M*X1, X2);
         are_inline = dists < t;
         n_inline = sum(are_inline);
+        %update N
+        oneMinusEpsilon = n_inline / num;
+        N = ceil(log(1 - p) / log(1 - oneMinusEpsilon ^ s));
         % update the consensus set if it is better
         if n_inline>nb_inline
             nb_inline = n_inline;
             X1_inline = X1(are_inline, :);
             X2_inline = X2(are_inline, :);
-        end
-        % if we find enough inliner we can stop
-        if n_inline>in_t
-            break
         end
         % increment trial number
         trial = trial + 1;
