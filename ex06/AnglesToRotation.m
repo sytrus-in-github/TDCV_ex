5148dd90f0ac5d2c11bd06fabfccab7bc66222af
Antoine Saporta <antwone.saporta@gmail.com>
2016-12-14 13:42:33 +0100

    Changed AnglesToRotation.m

--- ex06/AnglesToRotation.m
+++ ex06/AnglesToRotation.m
@@ -5,11 +5,11 @@ function R = AnglesToRotation( alpha, beta, gamma )
     sina = sin(alpha);
     sinb = sin(beta);
     sinc = sin(gamma);
-%     R = [cosa * cosc - sina * cosb * sinc , - cosa * sinc - sina * cosb * cosc , sina * sinc;
-%         sina * cosc + cosa * cosb * sinc , - sina * sinc + cosa * cosb * cosc , -cosa * sinc;
-%         sinb * sinc , sinb * cosc , cosb];
-    R = [cosa * cosb, - sina * cosc + cosa * sinb * sinc, sina * sinc + cosa * sinb * cosc;
-         sina * cosb, cosa * cosc + sina * sinb * sinc, - cosa * sinc + sina * sinb * cosc;
-         sinb, cosb * sinc, cosb * cosc];
+    R = [cosa * cosc - sina * sinb * sinc , -cosb * sina , sina * sinb * cosc + cosa * sinc;
+        sina * cosc + cosa * sinb * sinc, cosa * cosb, sina * sinc - cosa * sinb * cosc;
+        - cosb * sinc, sinb, cosb * cosc];
+%     R = [cosa * cosb, - sina * cosc + cosa * sinb * sinc, sina * sinc + cosa * sinb * cosc;
+%          sina * cosb, cosa * cosc + sina * sinb * sinc, - cosa * sinc + sina * sinb * cosc;
+%          sinb, cosb * sinc, cosb * cosc];
 end
 
